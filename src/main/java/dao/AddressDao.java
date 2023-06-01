c25d4862439162bc5aecf177685dbae02b50e782
JeongUm <wjddmagoxo@naver.com>
2023-06-01 17:43:52 +0900

    신정음/ AddressDao , CategoryDao(수정까지)
    AddressDao -> 리스트, 추가, 수정, 삭제
    CategoryDao -> 리스트, 추가, 수정

--- src/main/java/dao/AddressDao.java
+++ src/main/java/dao/AddressDao.java
@@ -1,5 +1,73 @@
 package dao;
 
-public class AddressDao {
+import java.sql.*;
+import java.util.*;
+import util.DBUtil;
+import vo.*;
 
+public class AddressDao {
+	//배송지이력 리스트 
+	public ArrayList<Address> selectHistoryAddress(String id) throws Exception {
+		DBUtil dbUtil = new DBUtil();
+		Connection conn = dbUtil.getConnection();
+		PreparedStatement addressStmt = conn.prepareStatement(
+				"SELECT address_no addressNo, id, address_lastdate addressLastdate, address FROM address WHERE id = ? ORDER BY address_lastdate DESC"
+		);
+		addressStmt.setString(1, id);
+		
+		ResultSet addressRs= addressStmt.executeQuery();
+		
+		//ArrayList
+		ArrayList<Address> addressList = new ArrayList<>();
+		while(addressRs.next()) {
+			Address a = new Address();
+			a.setAddressNo(addressRs.getInt("addressNo"));
+			a.setId(addressRs.getString("id"));
+			a.setAddressLastdate(addressRs.getString("addressLastdate"));
+			a.setAddress(addressRs.getString("address"));
+			addressList.add(a);
+		}
+		return addressList;
+	}
+	
+	//주소번호가 없으면 추가, 번호가 있을때는 마지막일정 업데이트
+	public int updateAddress(Address address) throws Exception {
+		if(address == null || address.getId() ==null || address.getAddress() == null) {
+			System.out.println("address클래스, id, 주소값 확인");
+			return 0;
+		}
+		int row = 0;
+		DBUtil dbUtil = new DBUtil();
+		Connection conn = dbUtil.getConnection();
+		if(address.getAddressNo() == 0) {
+			PreparedStatement addressStmt = conn.prepareStatement(
+					"INSERT INTO address(id, address_lastdate, address, createdate, updatedate) VALUES(?, now(), ?, now(), now())"
+					);
+			addressStmt.setString(1, address.getId());
+			addressStmt.setString(2, address.getAddress());
+			
+			row = addressStmt.executeUpdate();
+			return row;
+		} 
+		PreparedStatement addressUpStmt = conn.prepareStatement(
+				"UPDATE address SET address_lastdate = now() WHERE address_no = ?"
+				);
+		addressUpStmt.setInt(1, address.getAddressNo());
+		row = addressUpStmt.executeUpdate();
+		return row;
+	}
+	
+	//배송지이력 삭제
+	public int deleteAddress(int addressNo) throws Exception {
+		int row = 0;
+		DBUtil dbUtil = new DBUtil();
+		Connection conn = dbUtil.getConnection();
+		PreparedStatement addressdelStmt = conn.prepareStatement(
+					"DELETE FROM address WHERE address_no = ?"
+			);
+		addressdelStmt.setInt(1, addressNo);
+			
+		addressNo = addressdelStmt.executeUpdate();
+			return addressNo;
+		} 
 }
--- src/main/java/dao/CategoryDao.java
+++ src/main/java/dao/CategoryDao.java
@@ -1,5 +1,71 @@
 package dao;
 
-public class CategoryDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.*;
 
+import util.DBUtil;
+import vo.*;
+
+public class CategoryDao {
+	//카테고리 리스트
+	public ArrayList<Category> selectCategoryList() throws Exception {
+		DBUtil dbUtil = new DBUtil();
+		Connection conn = dbUtil.getConnection();
+		PreparedStatement categoryStmt = conn.prepareStatement(
+				"SELECT category_name categoryName, createdate, updatedate FROM category ORDER BY categoryName"
+			);
+		ResultSet categoryRs = categoryStmt.executeQuery();
+		
+		ArrayList<Category> categoryList = new ArrayList<Category>();
+		while(categoryRs.next()) {
+			Category c = new Category();
+			c.setCategoryName(categoryRs.getString("categoryName"));
+			c.setCreatedate(categoryRs.getString("createdate"));
+			c.setUpdatedate(categoryRs.getString("updatedate"));
+			categoryList.add(c);
+		}
+		return categoryList;
+	}
+	
+	//카테고리 추가
+	public int insertCategory(Category category) throws Exception {
+		int row = 0;
+		if(category == null || category.getCategoryName() == null) {
+			System.out.println("category클래스, name값 확인");
+			return 0;
+		}
+		DBUtil dbUtil = new DBUtil();
+		Connection conn = dbUtil.getConnection();
+		PreparedStatement categoryInsStmt = conn.prepareStatement(
+				"INSERT INTO category(category_name, createdate, updatedate) VALUES(?, now(), now())"
+			);
+		categoryInsStmt.setString(1, category.getCategoryName());
+		row = categoryInsStmt.executeUpdate();
+		
+		return row;
+	}
+	
+	//카테고리 수정
+	public int updateCategory(Category oriCategory, Category newCategory) throws Exception {
+		int row = 0;
+		
+		if(oriCategory == null || newCategory == null || 
+				oriCategory.getCategoryName() == null || newCategory.getCategoryName() == null ) {
+			System.out.println("category클래스, name값 확인");
+			return 0;
+		}
+		DBUtil dbUtil = new DBUtil();
+		Connection conn = dbUtil.getConnection();
+		PreparedStatement categoryUpStmt = conn.prepareStatement(
+				"UPDATE category SET category_name = ? WHERE category_name = ?"
+			);
+		categoryUpStmt.setString(1, newCategory.getCategoryName());
+		categoryUpStmt.setString(2, oriCategory.getCategoryName());
+		
+		row = categoryUpStmt.executeUpdate();
+		
+		return row;
+	}
 }
