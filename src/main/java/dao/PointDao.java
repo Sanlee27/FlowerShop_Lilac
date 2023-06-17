c8d12d354e447ad17d6669021906f874f8b526f2
Sanlee27 <010121qqq@naver.com>
2023-06-17 21:07:42 +0900

    이강산 / PointDao_주문취소시 적립/사용 포인트 취소 메소드 추가

--- src/main/java/dao/PointDao.java
+++ src/main/java/dao/PointDao.java
@@ -32,7 +32,7 @@ public class PointDao {
 		DBUtil dbUtil = new DBUtil(); 
 		Connection conn = dbUtil.getConnection();
 		// 고객 포인트 증감이력 출력
-		String sql = "SELECT o.id, p.point_no, p.order_no, p.point_pm, p.point, p.createdate FROM orders o	JOIN point_history p ON o.order_no = p.order_no	WHERE o.id = ?";
+		String sql = "SELECT o.id, p.point_no, p.order_no, p.point_pm, p.point, p.createdate FROM orders o	JOIN point_history p ON o.order_no = p.order_no	WHERE o.id = ? ORDER BY createdate DESC";
 		PreparedStatement stmt = conn.prepareStatement(sql);
 		stmt.setString(1, id);
 		
@@ -44,6 +44,7 @@ public class PointDao {
 			point.setPoint(rs.getInt("p.point"));
 			point.setCreatedate(rs.getString("p.createdate"));
 			
+			// 상품이름, 이미지 출력 쿼리
 			String productSql = "SELECT p.product_name, pi.product_save_filename FROM point_history ph JOIN orders o ON ph.order_no = o.order_no JOIN product p ON o.product_no = p.product_no JOIN product_img pi ON p.product_no = pi.product_no WHERE ph.order_no = ?";
 			PreparedStatement stmt2 = conn.prepareStatement(productSql);
 			stmt2.setInt(1, point.getOrderNo());
@@ -99,4 +100,44 @@ public class PointDao {
 	}
 	// 등급별 포인트 적립 반영은 jsp에서 처리
 	
+	// 주문취소시 적립 / 사용 포인트 취소 처리
+	public int cancelPoint(Point point, String id) throws Exception {
+		int row = 0;
+		// DB메소드
+		DBUtil dbUtil = new DBUtil(); 
+		Connection conn = dbUtil.getConnection();
+		// 새롭게 취소내역 추가
+		// 취소된 주문번호 조회
+		String selectSql = "SELECT * FROM point_history WHERE order_no = ?";
+		PreparedStatement selectStmt = conn.prepareStatement(selectSql);
+		selectStmt.setInt(1, point.getOrderNo()); // 조회할 order_no를 지정
+		ResultSet rs = selectStmt.executeQuery();
+		
+		if(rs.next()) {
+			int orderNo = rs.getInt("order_no");
+		    String prePm = rs.getString("point_pm");
+		    int prePoint = rs.getInt("point");
+
+		    // 반대로 할 부호 계산
+		    String cancelPm = (prePm.equals("+")) ? "-" : "+";
+
+		    // 새로운 데이터로 insert
+		    String cancelSql = "INSERT INTO point_history (order_no, point_pm, point, createdate) VALUES (?, ?, ?, NOW())";
+		    PreparedStatement stmt2 = conn.prepareStatement(cancelSql);
+		    stmt2.setInt(1, orderNo);
+		    stmt2.setString(2, cancelPm);
+		    stmt2.setInt(3, prePoint);
+		    
+		    row = stmt2.executeUpdate();
+		    
+		    int cancelPoint = cancelPm.equals("-") ? prePoint * -1 : prePoint;
+		    String cancelPointSql = "UPDATE customer SET cstm_point = cstm_point + ? WHERE id = ?";
+		    PreparedStatement stmt3 = conn.prepareStatement(cancelPointSql);
+		    stmt3.setInt(1, cancelPoint);
+		    stmt3.setString(2, id);
+		    
+		    row += stmt3.executeUpdate();
+		}
+		return row;
+	}
 }
